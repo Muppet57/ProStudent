8c1769e15fc45bb9b53ab8c70574f7921556289a
Muppet57 <jonnhy157@gmail.com>
2018-03-07 22:59:31 +0000

    Drawer e Home Page v2

--- app/src/main/java/com/student/pro/prostudent/Home.java
+++ app/src/main/java/com/student/pro/prostudent/Home.java
@@ -1,7 +1,6 @@
 package com.student.pro.prostudent;
 
 import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -16,10 +15,10 @@ public class Home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        mToolbar = (Toolbar) findViewById(R.id.nav_action);
+        mToolbar = findViewById(R.id.nav_action);
         setSupportActionBar(mToolbar);
 
-        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawerlayout);
+        mDrawerLayout = findViewById(R.id.drawerlayout);
         mToggle = new ActionBarDrawerToggle(this,mDrawerLayout,R.string.open,R.string.close);
         mDrawerLayout.addDrawerListener(mToggle);
         mToggle.syncState();
